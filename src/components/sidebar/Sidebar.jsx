a14397704c7f900a27be161ba22bdc481383eae4
Jean Santos <jeansantos.sw@gmail.com>
2023-01-24 16:28:56 +0000

    css

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,8 +1,31 @@
+import { PencilLine, PencilSimple } from 'phosphor-react'
+
 import styles from './Sidebar.module.css'
 
 export function Sidebar() {
   return (
-    <aside className={styles.sidebar}>~
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1522252234503-e356532cafd5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+        alt=""
+      />
+      <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://avatars.githubusercontent.com/u/43581104?v=4"
+          alt=""
+        />
+        <strong>Jean Santos</strong>
+        <span>Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar Perfil
+        </a>
+      </footer>
     </aside>
   )
 }
